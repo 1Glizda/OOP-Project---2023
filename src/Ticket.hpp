98650ca1701f39f00ecc1144ad94e5bdbb53c6dd
bobo <99484315+1Glizda@users.noreply.github.com>
2023-11-22 15:05:10 +0200

    Completed the classes constructors.

--- src/Ticket.hpp
+++ src/Ticket.hpp
@@ -4,5 +4,27 @@
 using namespace std;
 
 class Ticket {
+    const int ticketId;
+    bool isValid;
+    bool isVIP;
+    string district;
+    int validity;
+
+public:
+    int genRandomNumber() {
+	    srand((unsigned) time(NULL));
+	    int random = rand();
+	    return random;
+    }
+     Ticket() : ticketId(genRandomNumber()){
+        this->isValid = true;
+        this->isVIP = false;
+        this->district = "at home";
+        this->validity = 1;
+     };
+
+     void showObject() {
+        cout << "Ticket id: " << ticketId << "\nIs valid: " << isValid << "\nIs vip? " << isVIP << "\nAt what district? " << district << "\nHow many days is it valid? " << validity << endl << endl;
+            }
 
 };
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 
 int main() {
-    cout << "hello world" << endl << endl;
     Event e;
     Event e2("12-06-2002", "15:00", "bOb");
     e.showObject();
@@ -16,6 +15,8 @@ int main() {
     EventLocation el2(32, 8, Nord, 4);
     el.showObject();
     el2.showObject();
+    Ticket t;
+    t.showObject();
 
     return 0;
 }
